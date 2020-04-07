6a17e0d8c01e3999afadc7a2effe8aa9fc4c8294
GabrielZoppo <53881684+GabrielZoppo@users.noreply.github.com>
2020-04-07 18:50:22 -0300

    Update app.java

--- app.java
+++ app.java
@@ -1,10 +1,13 @@
 package app;
-
+import java.util.ArrayList;
 public class App{
-    public static void main(String[] args) throws Exception {
-        ContaCorrente gabriel = new ContaCorrente(5000);
-        gabriel.depositar(1500);
-        gabriel.sacar(2000);
-        system.out.println(gabriel.saldo);
+    public static void main(String[] args){
+        ContaCorrente[] Contas;
+        Contas = new ContaCorrente[10];
+        ContaCorrente Gabriel = new ContaCorrente();
+        
+        Gabriel.depositar(15000);
+        Gabriel.sacar(2000);
+        System.out.println(Gabriel.saldo);
     }
 }
