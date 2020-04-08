0c3f58a4d4d9102f8ba41d597c7c9cdf22aa3c21
GabrielZoppo <53881684+GabrielZoppo@users.noreply.github.com>
2020-04-07 21:13:41 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,8 +1,7 @@
-package app;
+ppackage Contas;
 
 public class ContaCorrente {
     private double saldo;
-    private double limite;
     
     public ContaCorrente(){
         this.saldo = 0;
@@ -10,11 +9,6 @@ public class ContaCorrente {
 
     public ContaCorrente(double valor){
         this.saldo = valor;
-    }
-
-    public ContaCorrente(double valor, double limite){
-        this.saldo = valor;
-        this.limite  = limite;
     }
 
     public double getSaldo(){
