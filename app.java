f7ad62b0078e80f8f4e48ce46d256eda2948373c
GabrielZoppo <53881684+GabrielZoppo@users.noreply.github.com>
2020-04-07 21:13:06 -0300

    Update app.java

--- app.java
+++ app.java
@@ -1,13 +1,15 @@
-package app;
-
+package Contas;
 import java.util.ArrayList;
 public class App{
     public static void main(String[] args){
+
+        ContaCorrente gabriel = new ContaCorrente(2000);
         ArrayList<ContaCorrente> contas = new ArrayList<ContaCorrente>();
         contas.add(gabriel);
 
+
         gabriel.depositar(15000);
         gabriel.sacar(2000);
-        System.out.println(contas.getsaldo(gabriel));
+        System.out.println(gabriel.getSaldo);
     }
 }
